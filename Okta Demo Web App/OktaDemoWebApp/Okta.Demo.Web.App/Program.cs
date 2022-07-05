84e8e5ac678ddd6b65b01c7e43e189d7fe8d9c65
Yaad Murshed <ymurshed@ePharmaSolutions.com>
2022-07-05 12:48:53 +0600

    Added Authentication.

--- Okta Demo Web App/OktaDemoWebApp/Okta.Demo.Web.App/Program.cs
+++ Okta Demo Web App/OktaDemoWebApp/Okta.Demo.Web.App/Program.cs
@@ -34,6 +34,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
